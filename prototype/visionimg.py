ee09b8dedd71a2faad29728694527ac27684f60a
Jason Hong <jason810@bu.edu>
2020-04-24 14:19:52 -0700

    diff img test

--- prototype/visionimg.py
+++ prototype/visionimg.py
@@ -9,7 +9,7 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = os.path.abspath('/home/jason/Downloads/cat.jpg')
+file_name = os.path.abspath('/home/jason/Downloads/angryman.jpg')
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
