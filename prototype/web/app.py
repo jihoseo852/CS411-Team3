bc620fc15d2b55b53aa545536babc3758f161dff
Jason Hong <jason810@bu.edu>
2020-04-25 14:48:32 -0700

    site upload works

--- prototype/web/app.py
+++ prototype/web/app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def fileFrontPage():
-	return render_template('fileform.html')
+	return render_template('./index.html')
+
+@app.route("/handleUpload", methods=['POST'])
+def fileUpload():
+	if 'photo' in request.files:
+		photo = request.files['photo']
+		if photo.filename != '':
+			photo.save(os.path.join('/home/jason/Documents/JH/projects/411/prototype/files', photo.filename))
+	return redirect(url_for('fileFrontPage'))
 
 if __name__ == '__main__':
 	app.run()
